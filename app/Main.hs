605107c5f56e8b1fce9b88f67d0941d663a4b918
Mark Hopkins <markjohnhopkins@gmail.com>
2024-11-02 16:16:11 +1100

    Validate module name; check if file already exists

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,80 @@
+{-# LANGUAGE ViewPatterns    #-}
+{-# LANGUAGE PatternSynonyms #-}
+
 module Main where
 
+import           Control.Monad
+import           Control.Monad.Trans.Except
+import           Data.Char (isSpace)
+import qualified Data.List as L
+import           Data.List (isPrefixOf)
+import           Data.Maybe
+import           Data.Tuple.Extra
 import           System.Environment (getArgs)
-import           System.IO
 import           System.Exit
 
-import CreateModule
+import           CreateModule
 
 main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [s]               -> newModule "src" s
-    ["-d", dir, s]    -> newModule dir s
-    ["--dir", dir, s] -> newModule dir s
-    [s, "-d", dir]    -> newModule dir s
-    [s, "--dir", dir] -> newModule dir s
-    _ -> do
-      hPutStrLn stderr usage
-      exitWith (ExitFailure 2)
+main = getArgs >>= maybe exitUsage (uncurry3 run) . processArgs
+  where
+    exitUsage = logStr "ERROR: could not parse arguments." >> logStr usage >> exitWith (ExitFailure 2)
+
+run :: FilePath -> Bool -> String -> IO ()
+run dir force = either handleErr return <=< runExceptT . newModule dir force . trim
+  where
+    handleErr e = do
+      logStr $ "ERROR: " ++ e
+      exitFailure
 
 usage :: String
 usage = unlines
   [ "Usage: new-module [options] FQN"
   , ""
-  , "Create a new empty Haskell module, creating directories if needed."
+  , "Create a new empty Haskell module, creating directories as required."
   , ""
   , "Command options:"
   , "  -d, --dir directory        Directory in which to create module (defaults to src);"
+  , "  -f, --force                Overwrite the file if it already exists;"
   ]
+
+-- | Returns (directory, force, module name)
+processArgs :: [String] -> Maybe (String, Bool, String)
+processArgs = go Nothing Nothing Nothing
+  where
+  go md mf (Just n) []                       = Just (fromMaybe "src" md, fromMaybe False mf, n)
+  go md mf Nothing  (NotOpt n:ss)            = go md mf (Just n) ss
+  go md mf mn       (Long "dir":NotOpt d:ss) = maybe (go (Just d) mf mn ss) (const Nothing) md
+  go md mf mn       (Short "d" :NotOpt d:ss) = maybe (go (Just d) mf mn ss) (const Nothing) md
+  go md mf mn       (Long "force":ss)        = maybe (go md (Just True) mn ss) (const Nothing) mf
+  go md mf mn       (Short "f":ss)           = maybe (go md (Just True) mn ss) (const Nothing) mf
+  go  _  _  _       _                        = Nothing
+
+pattern Long :: String -> String
+-- pattern Long s <- (long -> Just s) where Long = ("--" ++ )
+pattern Long s <- (long -> Just s) where Long = ("--" ++ )
+
+pattern Short :: String -> String
+pattern Short s <- (short -> Just s) where Short = ("-" ++)
+
+pattern NotOpt :: String -> String
+pattern NotOpt s <- (notOpt -> Just s) where NotOpt = id
+
+long :: String -> Maybe String
+long s
+  | "--" `isPrefixOf` s = Just $ L.drop 2 s
+  | otherwise           = Nothing
+
+short :: String -> Maybe String
+short s
+  | "-" `isPrefixOf` s = Just $ L.drop 1 s
+  | otherwise          = Nothing
+
+notOpt :: String -> Maybe String
+notOpt s
+  | "--" `isPrefixOf` s = Nothing
+  | "-" `isPrefixOf` s  = Nothing
+  | otherwise           = Just s
+
+trim :: String -> String
+trim = L.dropWhile isSpace . L.dropWhileEnd isSpace
--- src/CreateModule.hs
+++ src/CreateModule.hs
@@ -1,21 +1,82 @@
 module CreateModule where
 
+import           Control.Monad
+import           Control.Monad.IO.Class
+import           Control.Monad.Trans.Except
 import qualified Data.List as L
-import           System.Directory (createDirectoryIfMissing)
+import           System.Directory (createDirectoryIfMissing, doesFileExist, doesDirectoryExist, doesPathExist)
 import           System.FilePath
 import           System.IO
+import           Text.Regex.TDFA
 
-newModule :: FilePath -> String -> IO ()
-newModule dir nm = do
-  hPutStrLn stderr . unwords $ ["Creating new module", nm, "in directory", dir ++ "."]
-  let
-    contents =
-      unwords ["module", nm, "where"] ++ "\n\n"
-    path = dir </> map tr nm ++ ".hs"
+newModule :: FilePath -> Bool -> String -> ExceptT String IO ()
+newModule dir force modName = do
+  checkValidName modName
+  let path = nameToPath dir modName
+  checkIfPathExists path force
+  logStr . unwords $ ["Creating new module", modName, "in directory", dir ++ "."]
+  liftIO $ createDirectoryIfMissing True (parent path)
+  liftIO $ writeFile path (moduleContents modName)
+
+checkValidName :: Monad m => [Char] -> ExceptT String m ()
+checkValidName name
+  = unless (isValidModuleName name)
+  . throwE
+  . unwords
+  $ ["\"" ++ name ++ "\"", "is not a valid module name."]
+
+nameToPath :: FilePath -> String -> FilePath
+nameToPath dir name = dir </> map tr name ++ ".hs"
+  where
     tr '.' = '/'
     tr ch  = ch
-  createDirectoryIfMissing True (parent path)
-  writeFile path contents
+
+checkIfPathExists :: MonadIO m => FilePath -> Bool -> ExceptT String m ()
+checkIfPathExists path force = do
+  pathExists <- liftIO $ doesPathExist path
+  when (pathExists && not force) $ do
+    dirExists  <- liftIO $ doesDirectoryExist path
+    fileExists <- liftIO $ doesFileExist path
+    let ptype = case (dirExists, fileExists) of
+          (True, _)      -> "Directory"
+          (_, True)      -> "File"
+          (False, False) -> "Path"
+    let msg = [ptype, path, "already exists. Not overwriting without --force."]
+    throwE . unwords $ msg
+
+moduleContents :: String -> String
+moduleContents nm = unwords ["module", nm, "where"] ++ "\n\n"
+
+{- | NB. Unicode not currently supported. -}
+isValidModuleName :: String -> Bool
+isValidModuleName s = s =~ anchor modRegex
+
+anchor :: String -> String
+anchor r = "\\`" ++ r ++ "\\'"
+
+modRegex :: String
+modRegex = cmpRegex ++ "(\\." ++ cmpRegex ++ ")*"
+
+cmpRegex :: String
+cmpRegex = "([A-Z][A-Za-z0-9_']*)"
+
+{-
+From the Haskell 2010 Report:
+
+conid    → large {small | large | digit | '}
+modid    → {conid.}conid
+large    → ascLarge | uniLarge
+ascLarge → A|B|..|Z
+uniLarge → any uppercase or titlecase Unicode letter
+small    → ascSmall | uniSmall | _
+ascSmall → a|b|..|z
+uniSmall → any Unicode lowercase letter
+digit    → ascDigit | uniDigit
+ascDigit → 0 | 1 | . . . | 9
+uniDigit → any Unicode decimal digit
+
+Not currently making any attempt to support Unicode.
+-}
 
 pathComponents :: FilePath -> [String]
 pathComponents = go []
@@ -30,3 +91,6 @@ parent :: FilePath -> FilePath
 parent fp = case pathComponents fp of
   l@(_:_) -> L.intercalate "/" $ init l
   _       -> error $ "Can't get parent of " ++ fp
+
+logStr :: MonadIO m => String -> m ()
+logStr = liftIO . hPutStrLn stderr
